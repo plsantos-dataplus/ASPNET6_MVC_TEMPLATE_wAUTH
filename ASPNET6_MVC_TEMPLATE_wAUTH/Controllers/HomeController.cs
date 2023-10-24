32a7c8dec1bbc800f7b8b63ffffe818681825ae6
Patrick_Santos <patrick.lloyd.santos@gmail.com>
2023-10-24 11:29:35 +0800

    Add project files.

---
+++ ASPNET6_MVC_TEMPLATE_wAUTH/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using ASPNET6_MVC_TEMPLATE_wAUTH.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ASPNET6_MVC_TEMPLATE_wAUTH.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
